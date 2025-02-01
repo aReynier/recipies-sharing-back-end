fc3d8348a5a92e2c1c63fc74406b0d153eabaccc
Aurore Reynier <reynier.aurore@gmail.com>
2025-02-01 12:57:13 +0100

    ci(workflow): add healthcheck controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,13 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // Ajouté pour le healthcheck docker
+  @Get('health')
+  healthCheck() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString()
+    };
+  }
 }
